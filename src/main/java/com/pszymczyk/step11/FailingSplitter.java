9442512b478aaec861a270fb93e1c531a879c087
Pawel Szymczyk <pawel@pszymczyk.com>
2021-05-08 15:37:17 +0200

    read commited vs read uncommited

--- src/main/java/com/pszymczyk/step11/FailingSplitter.java
+++ src/main/java/com/pszymczyk/step11/FailingSplitter.java
@@ -1,4 +1,5 @@
-package com.pszymczyk.step10;
+package com.pszymczyk.step11;
+
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,8 +13,9 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,28 +26,27 @@ import java.util.Properties;
 import java.util.Random;
 
 
-public class LoanApplicationProcess implements Runnable {
+public class FailingSplitter implements Runnable {
 
-    private final KafkaConsumer<String, LoanApplicationRequest> consumer;
-    private final KafkaProducer<String, LoanApplicationDecision> producer;
+    protected static Logger logger = LoggerFactory.getLogger(FailingSplitter.class);
+
+    private final KafkaConsumer<String, String> consumer;
+    private final KafkaProducer<String, String> producer;
     private final String loanApplicationRequestsTopic;
     private final String loanApplicationDecisionsTopic;
     private final String groupId;
-    private final DebtorsRepository debtorsRepository;
 
 
-    public LoanApplicationProcess(String loanApplicationRequestsTopic, String loanApplicationDecisionsTopic, String groupId,
-                                  DebtorsRepository debtorsRepository) {
+    public FailingSplitter(String loanApplicationRequestsTopic, String loanApplicationDecisionsTopic, String groupId) {
         this.loanApplicationRequestsTopic = loanApplicationRequestsTopic;
         this.loanApplicationDecisionsTopic = loanApplicationDecisionsTopic;
         this.groupId = groupId;
-        this.debtorsRepository = debtorsRepository;
 
         Properties consumerProperties = new Properties();
         consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LoanApplicationDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         this.consumer = new KafkaConsumer<>(consumerProperties);
@@ -53,7 +54,7 @@ public class LoanApplicationProcess implements Runnable {
         Properties producerProperties = new Properties();
         producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LoanApplicationDecisionSerializer.class.getName());
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerProperties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         producerProperties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "rental_car_process_1");
         this.producer = new KafkaProducer<>(producerProperties);
@@ -66,21 +67,18 @@ public class LoanApplicationProcess implements Runnable {
         try {
             consumer.subscribe(Arrays.asList(loanApplicationRequestsTopic));
             while (true) {
-                ConsumerRecords<String, LoanApplicationRequest> loanApplicationRequests = consumer.poll(Duration.ofMillis(Long.MAX_VALUE));
+                ConsumerRecords<String, String> loanApplicationRequests = consumer.poll(Duration.ofMillis(Long.MAX_VALUE));
                 if (!loanApplicationRequests.isEmpty()) {
                     producer.beginTransaction();
                     try {
-                        List<ProducerRecord<String, LoanApplicationDecision>> outputRecords = processApplications(loanApplicationRequests);
-                        for (ProducerRecord<String, LoanApplicationDecision> outputRecord : outputRecords) {
+                        List<ProducerRecord<String, String>> outputRecords = processApplications(loanApplicationRequests);
+                        for (ProducerRecord<String, String> outputRecord : outputRecords) {
                             producer.send(outputRecord);
                         }
                         producer.sendOffsetsToTransaction(getUncommittedOffsets(loanApplicationRequests), groupId);
-                        Random rand = new Random();
-                        int randomNum = rand.nextInt((4 - 1) + 1) + 1;
-                        System.err.println(randomNum);
-                        if (randomNum == 2) {
-                            throw new Exception();
-                        }
+
+                        failSometimes();
+
                         producer.commitTransaction();
                     } catch (Exception e) {
                         producer.abortTransaction();
@@ -98,44 +96,36 @@ public class LoanApplicationProcess implements Runnable {
         }
     }
 
-    private Map<TopicPartition, OffsetAndMetadata> getUncommittedOffsets(ConsumerRecords<String, LoanApplicationRequest> records) {
+    private void failSometimes() throws Exception {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((4 - 1) + 1) + 1;
+        logger.info("Random number: {}", randomNum);
+        if (randomNum == 2) {
+            throw new Exception();
+        }
+    }
+
+    private Map<TopicPartition, OffsetAndMetadata> getUncommittedOffsets(ConsumerRecords<String, String> records) {
         Map<TopicPartition, OffsetAndMetadata> offsetsToCommit = new HashMap<>();
         for (TopicPartition partition : records.partitions()) {
-            List<ConsumerRecord<String, LoanApplicationRequest>> partitionedRecords = records.records(partition);
+            List<ConsumerRecord<String, String>> partitionedRecords = records.records(partition);
             long offset = partitionedRecords.get(partitionedRecords.size() - 1).offset();
             offsetsToCommit.put(partition, new OffsetAndMetadata(offset + 1));
         }
         return offsetsToCommit;
     }
 
-    private List<ProducerRecord<String, LoanApplicationDecision>> processApplications(ConsumerRecords<String, LoanApplicationRequest> loanApplicationRequests) {
-        List<ProducerRecord<String, LoanApplicationDecision>> loanApplicationDecisions = new ArrayList<>();
-        for (ConsumerRecord<String, LoanApplicationRequest> loanApplicationRequestConsumerRecord : loanApplicationRequests) {
-            LoanApplicationRequest loanApplicationRequest = loanApplicationRequestConsumerRecord.value();
-            if (debtorsRepository.getDebtors().contains(loanApplicationRequest.getRequester())) {
-                submitDecision(loanApplicationDecisions, loanApplicationRequest.getAmount().multiply(new BigDecimal("0.5")),
-                    loanApplicationRequest.getRequester());
-            } else if (debtorsRepository.getBlackList().contains(loanApplicationRequest.getRequester())) {
-                submitDecision(loanApplicationDecisions, BigDecimal.ZERO, loanApplicationRequest.getRequester());
-            } else {
-                submitDecision(loanApplicationDecisions, loanApplicationRequest.getAmount(), loanApplicationRequest.getRequester());
+    private List<ProducerRecord<String, String>> processApplications(ConsumerRecords<String, String> loanApplicationRequests) {
+        List<ProducerRecord<String, String>> producerRecords = new ArrayList<>();
+        for (ConsumerRecord<String, String> record : loanApplicationRequests) {
+            for (String singleChar: record.value().split("\\.")) {
+                ProducerRecord<String, String> loanApplicationDecisionProducerRecord = new ProducerRecord<>(loanApplicationDecisionsTopic, singleChar);
+                producerRecords.add(loanApplicationDecisionProducerRecord);
             }
         }
 
-        return loanApplicationDecisions;
+        return producerRecords;
     }
-
-    void submitDecision(List<ProducerRecord<String, LoanApplicationDecision>> loanApplicationDecisions,
-                        BigDecimal amount,
-                        String requester) {
-        LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
-        loanApplicationDecision.setAmount(amount);
-        loanApplicationDecision.setRequester(requester);
-        ProducerRecord<String, LoanApplicationDecision> loanApplicationDecisionProducerRecord = new ProducerRecord<>(loanApplicationDecisionsTopic,
-            loanApplicationDecision);
-        loanApplicationDecisions.add(loanApplicationDecisionProducerRecord);
-    }
-
 
     public void shutdown() {
         consumer.wakeup();
