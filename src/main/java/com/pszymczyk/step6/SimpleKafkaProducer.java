4c2369f28cd6487cbd3b0923d110cc490e72fac0
Pawel Szymczyk <pawel.szymczyk@displate.com>
2023-06-23 14:55:34 +0200

    schema registry

--- src/main/java/com/pszymczyk/step6/SimpleKafkaProducer.java
+++ src/main/java/com/pszymczyk/step6/SimpleKafkaProducer.java
@@ -1,42 +1,41 @@
 package com.pszymczyk.step6;
 
-import org.apache.kafka.clients.producer.*;
+import io.confluent.examples.clients.basicavro.Payment;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleKafkaProducer {
 
     private static final Logger logger = LoggerFactory.getLogger(SimpleKafkaProducer.class);
 
-    private final KafkaProducer<String, String> kafkaProducer;
+    private final KafkaProducer<String, Payment> kafkaProducer;
     private final String topic;
 
     public SimpleKafkaProducer(String topic) {
         var producerProperties = new Properties();
         producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        producerProperties.put("schema.registry.url","http://localhost:8081");
         this.kafkaProducer = new KafkaProducer<>(producerProperties);
         this.topic = topic;
     }
 
-    public void sendAndForget(String messageValue) {
-        var record = new ProducerRecord<String, String>(topic, messageValue);
-        try {
-            kafkaProducer.send(record);
-        } catch (Exception e) {
-            logger.error("Exception while sending message in send and forget mode", e);
-        }
-    }
 
     public RecordMetadata syncSend(String messageValue, long timeout, TimeUnit timeUnit) {
-        var record = new ProducerRecord<String, String>(topic, messageValue);
+        Payment payment = new Payment();
+        payment.setId("123");
+        payment.setAmount(1000.2);
+        var record = new ProducerRecord<String, Payment>(topic, payment);
         try {
             return kafkaProducer.send(record).get(timeout, timeUnit);
         } catch (Exception e) {
@@ -45,13 +44,4 @@ public class SimpleKafkaProducer {
         }
     }
 
-    public Future<RecordMetadata> asyncSend(String messageValue, Callback callback) {
-        var record = new ProducerRecord<String, String>(topic, messageValue);
-        try {
-            return kafkaProducer.send(record, callback);
-        } catch (Exception e) {
-            logger.error("Exception while sending message in async mode", e);
-            return CompletableFuture.completedFuture(null);
-        }
-    }
 }
--- src/main/java/com/pszymczyk/step6/Step6Runner.java
+++ src/main/java/com/pszymczyk/step6/Step6Runner.java
@@ -13,19 +13,10 @@ public class Step6Runner {
         var topic = "step6";
         var simpleKafkaProducer = new SimpleKafkaProducer(topic);
 
-        simpleKafkaProducer.sendAndForget("Message sent in send and forget mode");
 
         var messageSentInSyncMode = simpleKafkaProducer.syncSend("Message sent in sync mode", 5, TimeUnit.SECONDS);
         assert messageSentInSyncMode != null;
         logger.info("Message sent in sync mode metadata: {}", messageSentInSyncMode);
 
-        var messageSentInAsyncMode = simpleKafkaProducer.asyncSend("Message sent in async mode", (metadata, exception) -> {
-            logger.info("Message sent in async mode metadata: {}", metadata);
-            logger.info("Message sent in async mode exception: ", exception);
-        });
-
-        while (!messageSentInAsyncMode.isDone()) {
-            Thread.sleep(100);
-        }
     }
 }
